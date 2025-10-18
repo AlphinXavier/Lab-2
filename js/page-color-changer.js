5686d73adef2d50c72d70ee213db62e52f8517d4
Alphin Xavier <alphinxavier10@gmail.com>
2025-10-18 03:25:04 -0400

    Grabbed sliders and body element from the HTML

---
+++ js/page-color-changer.js
@@ -0,0 +1,7 @@
+// get the 3 range sliders from the HTML by their IDs
+const redSlider = document.getElementById("red");
+const greenSlider = document.getElementById("green");
+const blueSlider = document.getElementById("blue");
+
+// get the body element so we can change its background
+const pageBody = document.body;
