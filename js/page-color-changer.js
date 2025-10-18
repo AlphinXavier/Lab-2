d892f5ba9fd315f2831b154eee9209e96de1d9a6
Alphin Xavier <alphinxavier10@gmail.com>
2025-10-18 03:25:32 -0400

    Created function to update background color based on slider values

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -5,3 +5,20 @@ const blueSlider = document.getElementById("blue");
 
 // get the body element so we can change its background
 const pageBody = document.body;
+
+// this function updates the background color based on the slider values
+function updateBackgroundColor() {
+  // read the current values from each slider
+  const r = redSlider.value;
+  const g = greenSlider.value;
+  const b = blueSlider.value;
+
+  // build the RGB color string (ex: "rgb(255, 100, 50)")
+  const rgbColor = `rgb(${r}, ${g}, ${b})`;
+
+  // apply it to the background color of the page
+  pageBody.style.backgroundColor = rgbColor;
+
+  // log the color in the console just to check it’s working
+  console.log("Background color changed to:", rgbColor);
+}
